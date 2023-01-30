c73852650912a507f5b23f3d6a82e9f75635b85e
AP696 <addyphilips66@gmail.com>
2023-01-30 16:28:46 +0000

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,17 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
-
-/*=============== REMOVE MENU MOBILE ===============*/
-
-
-/*=============== SWIPER PROJECTS ===============*/
-
-
-/*=============== SWIPER TESTIMONIAL ===============*/
-
-
-/*=============== EMAIL JS ===============*/
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
-
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
